5d5e6a423d916ffaaee9a309ec4a8e74f118f8ce
JonBester <34302929+JonBester@users.noreply.github.com>
2017-12-06 20:09:05 +1000

    Add files via upload

---
+++ TATATAG.2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  4 11:01:26 2017
+
+@author: jonathanbester
+"""
+
+##Training Sequences
+
+TTTGCCGACTGAACGCATACGGTATTTTACCTTCCCACTCAAGAAAACTTATCTTATTCC
